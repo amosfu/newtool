1281436b9c8992d441598cd55176deb87d0bfa28
shuai <fs1984@msn.com>
2017-04-12 16:41:00 -0400

    More logs

--- src/concordia/ca/INSE6140/Utils.java
+++ src/concordia/ca/INSE6140/Utils.java
@@ -14,8 +14,9 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.mail.*;  
-import javax.mail.internet.*;  
+import javax.mail.*;
+import javax.mail.internet.*;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -26,12 +27,12 @@ import org.apache.logging.log4j.LogManager;
 public class Utils {
     public static final File STATIC_TEMP = new File("./static_temp.txt");
     public static final String USAGE = "Usage: (static|dynamic) (source code/log path)";
-    private static final String[] mailRecipients = {"cerivas@gmail.com","fs1984@msn.com","vijay.satti@live.com"};
-    private static final String from = "cerivas@gmail.com";  
-    
+    private static final String[] mailRecipients = {"cerivas@gmail.com", "fs1984@msn.com", "vijay.satti@live.com"};
+    private static final String from = "cerivas@gmail.com";
+
     private static final String REGEX = "(INSERT |UPDATE |SELECT |WITH |DELETE )([^;]*)";
     private static final Pattern PATTERN = Pattern.compile(REGEX);
-    
+
     private static final Logger logger = LogManager.getLogger(Utils.class);
 
     public static void extractSQLFromCode(File srcDir) throws IOException {
@@ -54,151 +55,157 @@ public class Utils {
         Utils.logMessage("SQL count: " + counter);
     }
 
-    public static Map<Integer,String> loadStaticAnalysisLog(File staticLogFile) throws IOException  {
+    public static Map<Integer, String> loadStaticAnalysisLog(File staticLogFile) throws IOException {
         if (!staticLogFile.exists()) {
-        	Utils.logMessage( "static_temp.txt doesn't exist!\n" + Utils.USAGE);
-        	return null;
+            Utils.logMessage("static_temp.txt doesn't exist!\n" + Utils.USAGE);
+            return null;
         }
-        Map<Integer,String> hashFile = new HashMap<>();
+        Map<Integer, String> hashFile = new HashMap<>();
         BufferedReader tempReader = new BufferedReader(new FileReader(staticLogFile));
         for (String sqlLine = tempReader.readLine(); null != sqlLine; sqlLine = tempReader.readLine()) {
-            hashFile.put(sqlLine.hashCode(),sqlLine);
+            hashFile.put(sqlLine.hashCode(), sqlLine);
         }
         tempReader.close();
 
         return hashFile;
     }
 
-    public static boolean processDynamicQuery(String logEntry, Map<Integer,String> staticQueryRepo)  {
+    public static boolean processDynamicQuery(String logEntry, Map<Integer, String> staticQueryRepo) {
 
-    	boolean possibleAttack = false;
-    	
-		if( logEntry != null ) {
-			//Query found.
+        boolean possibleAttack = false;
+
+        if (logEntry != null) {
+            //Query found.
             String[] components = logEntry.split("<<\\|[O|P]{0,1}>>");
-            Utils.logMessage("processDynamicQuery: " +components.length + " components found: ");
+            Utils.logMessage("processDynamicQuery: " + components.length + " components found: ");
             Utils.logMessage(Arrays.toString(components));
-            
-            if( components.length == 6) {
-            	Utils.logMessage("processDynamicQuery: 6 components");
+
+            if (components.length == 6) {
+                Utils.logMessage("processDynamicQuery: 6 components");
                 String suspiciousSQLTemp = components[components.length - 2];
                 String preparedSQL = components[components.length - 1];
                 String canonicalSQLTemplate = generateSQLTemplate(preparedSQL).get(0);
-    			String templateQuery = staticQueryRepo.get(canonicalSQLTemplate.hashCode());
-    			Utils.logMessage("processDynamicQuery: found queries: ");
-            	Utils.logMessage("processDynamicQuery: suspiciousSQLTemp:");
-            	Utils.logMessage(suspiciousSQLTemp);
-            	Utils.logMessage("processDynamicQuery: preparedSQL:");
-            	Utils.logMessage(preparedSQL);
-            	Utils.logMessage("processDynamicQuery: canonicalSQLTemplate:");
-            	Utils.logMessage(canonicalSQLTemplate);
-            	Utils.logMessage("processDynamicQuery: templateQuery:");
-            	Utils.logMessage(templateQuery);
+                String templateQuery = staticQueryRepo.get(canonicalSQLTemplate.hashCode());
+                Utils.logMessage("processDynamicQuery: found queries: ");
+                Utils.logMessage("processDynamicQuery: suspiciousSQLTemp:");
+                Utils.logMessage(suspiciousSQLTemp);
+                Utils.logMessage("processDynamicQuery: preparedSQL:");
+                Utils.logMessage(preparedSQL);
+                Utils.logMessage("processDynamicQuery: canonicalSQLTemplate:");
+                Utils.logMessage(canonicalSQLTemplate);
+                Utils.logMessage("processDynamicQuery: templateQuery:");
+                Utils.logMessage(templateQuery);
                 if (templateQuery == null) {
                     Utils.logMessage("processDynamicQuery: Not found on the repo... the query is suspicious.");
-                    List<String> potentialTempSQLs= generateSQLTemplate(suspiciousSQLTemp);
-                    Utils.logMessage("processDynamicQuery: potentialTempSQLs:");
-                    Utils.logMessage(potentialTempSQLs != null? Arrays.toString(potentialTempSQLs.toArray()) : "null");
-                    if(!potentialTempSQLs.isEmpty()){
-                        suspiciousSQLTemp = potentialTempSQLs.get(0);
-                    }
-                    if (!suspiciousSQLTemp.equalsIgnoreCase(canonicalSQLTemplate)) {
-                    	possibleAttack = true;
-                    	Utils.logMessage(canonicalSQLTemplate);
-                    	Utils.logMessage("Attack found!\n" + canonicalSQLTemplate);
-                    }
+
+                    possibleAttack = true;
+                    Utils.logMessage(canonicalSQLTemplate);
+                    Utils.logMessage("Attack found!\n" + canonicalSQLTemplate);
+
                 }
-            }
-            else if( components.length == 5) {
-            	Utils.logMessage("processDynamicQuery: 5 components");
+            } else if (components.length == 5) {
+                Utils.logMessage("processDynamicQuery: 5 components");
                 String suspiciousSQLTemp = components[components.length - 1];
                 String canonicalSQLTemplate = generateSQLTemplate(suspiciousSQLTemp).get(0);
-    			String templateQuery = staticQueryRepo.get(canonicalSQLTemplate.hashCode());
-    			Utils.logMessage("processDynamicQuery: found queries: ");
-            	Utils.logMessage("processDynamicQuery: suspiciousSQLTemp:");
-            	Utils.logMessage(suspiciousSQLTemp);
-            	Utils.logMessage("processDynamicQuery: canonicalSQLTemplate:");
-            	Utils.logMessage(canonicalSQLTemplate);
-            	Utils.logMessage("processDynamicQuery: templateQuery:");
-            	Utils.logMessage(templateQuery);
+                String templateQuery = staticQueryRepo.get(canonicalSQLTemplate.hashCode());
+                Utils.logMessage("processDynamicQuery: found queries: ");
+                Utils.logMessage("processDynamicQuery: suspiciousSQLTemp:");
+                Utils.logMessage(suspiciousSQLTemp);
+                Utils.logMessage("processDynamicQuery: canonicalSQLTemplate:");
+                Utils.logMessage(canonicalSQLTemplate);
+                Utils.logMessage("processDynamicQuery: templateQuery:");
+                Utils.logMessage(templateQuery);
                 if (templateQuery == null) { //Not found on the repo... the query is suspicious
                     Utils.logMessage("processDynamicQuery: Not found on the repo... the query is suspicious.");
-                    List<String> potentialTempSQLs= generateSQLTemplate(suspiciousSQLTemp);
-                    Utils.logMessage("processDynamicQuery: potentialTempSQLs:");
-                    Utils.logMessage(potentialTempSQLs != null? Arrays.toString(potentialTempSQLs.toArray()) : "null");
-                    if(!potentialTempSQLs.isEmpty()){
-                        suspiciousSQLTemp = potentialTempSQLs.get(0);
-                    }
-                    if (!suspiciousSQLTemp.equalsIgnoreCase(canonicalSQLTemplate)) {
-                    	possibleAttack = true;
-                    	Utils.logMessage(canonicalSQLTemplate);
-                    	Utils.logMessage("Attack found!\n" + canonicalSQLTemplate);
-                    }
+
+                    possibleAttack = true;
+                    Utils.logMessage(canonicalSQLTemplate);
+                    Utils.logMessage("Attack found!\n" + canonicalSQLTemplate);
+
                 }
             }
-		}
-    	
+        }
+
         return possibleAttack;
     }
-    
-	public static boolean notifyPossibleAttack(String suspiciousLogEntry) {
-	  
-		boolean result = false;
-	      Properties properties = System.getProperties();  
-	      properties.setProperty("mail.smtp.host", "localhost");  
-	      Session session = Session.getDefaultInstance(properties);  
-	  
-	      try{  
-	         MimeMessage message = new MimeMessage(session);  
-	         message.setFrom(new InternetAddress(from));  
-	         for( String recipient : mailRecipients ) {
-	        	 message.addRecipient(Message.RecipientType.TO,new InternetAddress(recipient));
-	         }
-	         message.setSubject("Possible attack:");  
-	         message.setText(String.format("Hello, we have detected a possible SQL injection attack.\n\nDetails: %s", suspiciousLogEntry));  
-	  
-	         // Send message  
-	         Transport.send(message);  
-	         Utils.logMessage("notifyPossibleAttack: message sent successfully...."); 
-	         result = true;
-	  
-	      }catch (MessagingException mex) {mex.printStackTrace();}  
-	      
-	      return result;
-	}
-	
-	public static Map<Long,String> getLogLastLine(File dynamicLogFile, long previousTime) {
-		Map<Long,String> result = new HashMap<>();
-		result.put(-1L, ""); //default
 
-		try (@SuppressWarnings("deprecation")
-		ReversedLinesFileReader reader = new ReversedLinesFileReader(dynamicLogFile)) {
-	        String line = null;
-	        String previousLine = null;
-	        while ((line = reader.readLine()) != null ) {
-	        	int idx = line.indexOf(SqlFormatPrettyfier.GENERAL_SEPARATOR);
-	            if(idx > -1) {
-	            	long currentVal = Long.parseLong(line.substring(0, idx));
-	            	if( currentVal <= previousTime ) {
-	            		if( previousLine == null ) {
-	            			previousLine = line;
-	            		}
-	            		idx = previousLine.indexOf(SqlFormatPrettyfier.GENERAL_SEPARATOR);	            			
-	            		long time = Long.parseLong(previousLine.substring(0, idx));
-	            		result.clear();
-	            		result.put(time, previousLine);
-	            		break;
-	            	}
-	            }
-            	previousLine = line;
-	        }
-	    } catch (IOException e) {
-	    	Utils.logMessage("Cannot get the last time output in log.");
-	    	Utils.logMessage(e.getStackTrace().toString(),Type.ERROR);
-		}		
-		
-		return result;
-	}
+    public static boolean notifyPossibleAttack(String suspiciousLogEntry) {
+
+        boolean result = false;
+
+        final String username = "username@gmail.com";
+        final String password = "password";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+
+//        Properties properties = System.getProperties();
+//        properties.setProperty("mail.smtp.host", "localhost");
+//        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            for (String recipient : mailRecipients) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            }
+            message.setSubject("Possible attack:");
+            message.setText(String.format("Hello, we have detected a possible SQL injection attack.\n\nDetails: %s", suspiciousLogEntry));
+
+            // Send message
+            Transport.send(message);
+            Utils.logMessage("notifyPossibleAttack: message sent successfully....");
+            result = true;
+
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static Map<Long, String> getLogLastLine(File dynamicLogFile, long previousTime) {
+        Map<Long, String> result = new HashMap<>();
+        result.put(-1L, ""); //default
+
+        try (@SuppressWarnings("deprecation")
+             ReversedLinesFileReader reader = new ReversedLinesFileReader(dynamicLogFile)) {
+            String line = null;
+            String previousLine = null;
+            while ((line = reader.readLine()) != null) {
+                int idx = line.indexOf(SqlFormatPrettyfier.GENERAL_SEPARATOR);
+                if (idx > -1) {
+                    long currentVal = Long.parseLong(line.substring(0, idx));
+                    if (currentVal <= previousTime) {
+                        if (previousLine == null) {
+                            previousLine = line;
+                        }
+                        idx = previousLine.indexOf(SqlFormatPrettyfier.GENERAL_SEPARATOR);
+                        long time = Long.parseLong(previousLine.substring(0, idx));
+                        result.clear();
+                        result.put(time, previousLine);
+                        break;
+                    }
+                }
+                previousLine = line;
+            }
+        } catch (IOException e) {
+            Utils.logMessage("Cannot get the last time output in log.");
+            Utils.logMessage(e.getStackTrace().toString(), Type.ERROR);
+        }
+
+        return result;
+    }
 
 /*
     public static boolean extractSQLFromLog(File dbLogFile) throws IOException, InterruptedException {
@@ -262,29 +269,28 @@ public class Utils {
     }
 
     public static void printUsage() {
-    	Utils.logMessage(USAGE);
+        Utils.logMessage(USAGE);
     }
-    
+
     public enum Type {
-    	INFO,
-    	ERROR
+        INFO,
+        ERROR
     }
-    
+
     public static void logMessage(String message, Type... type) {
-    	Type t;
-    	if( type.length == 0 ) 
-    		t = Type.INFO;
-    	else
-    		t = type[0];
-    	
-    	if( t == Type.INFO) {
-    		logger.info(message);
-    		System.out.println(message);
-    	}
-    	else {
-    		logger.error(message);
-    		System.err.println(message);
-    	}
+        Type t;
+        if (type.length == 0)
+            t = Type.INFO;
+        else
+            t = type[0];
+
+        if (t == Type.INFO) {
+            logger.info(message);
+            System.out.println(message);
+        } else {
+            logger.error(message);
+            System.err.println(message);
+        }
     }
-    
+
 }
